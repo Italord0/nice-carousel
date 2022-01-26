a02ccae4d5f85a1d4f4cbaed37a4ce88e2b60b07
italo <italord.melo@gmail.com>
2022-01-26 02:34:02 -0300

    Adding viewbinding, creating carousel component layout file with simple viewpager

--- nice-carousel/src/main/java/com/italo/nice_carousel/NiceCarousel.kt
+++ nice-carousel/src/main/java/com/italo/nice_carousel/NiceCarousel.kt
@@ -2,12 +2,28 @@ package com.italo.nice_carousel
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.italo.nice_carousel.databinding.NiceCarouselBinding
 
 class NiceCarousel @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding: NiceCarouselBinding by lazy {
+        val contextTheme = ContextThemeWrapper(context, R.style.Theme_MaterialComponents)
+        NiceCarouselBinding.inflate(LayoutInflater.from(contextTheme), this)
+    }
+
+    init {
+        setLayout()
+    }
+
+    private fun setLayout() {
+
+    }
 
 }
